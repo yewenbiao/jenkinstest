832fe0e4d929eebdf35e0830463afe62d88ac1cd
sdss <sssss>
2019-01-24 18:01:55 +0800

    tijiao

--- scripts/test_functons.py
+++ scripts/test_functons.py
@@ -1,21 +1,34 @@
 import allure
+import pytest
 
 
 def test_a():
     print("[aasfsdafas")
     assert 1
 
+@allure.step(title = '前置条件')
+@pytest.fixture()
+def qianzhitiaojian():
+    allure.attach("前置条件","加载驱动")
+    print("testestsete前置条件")
+
 @allure.step(title = '测试登录的流程')
-def test_b():
-    allure.attach("登录","输入用户名")
+@pytest.mark.parametrize("content",["登录1"])
+def test_b(content,qianzhitiaojian):
+    allure.attach(content,"输入用户名")
     print("sdfsdfsdfsd") #找登录名
-    allure.attach("登录", "输入密码")
+    allure.attach(content, "输入密码")
     print("sdfsdfsdfsd")  # 找密码
-    allure.attach("登录", "点击登录")
+    allure.attach(content, "点击登录")
     print("sdfsdfsdfsd")  # 登录
     allure.attach("登录", "在来一步")
     print("testsetset")
-    assert 0
+    assert 1
+
+
+@pytest.fixture()
+def houzhitiaojian():
+    print("后置条件")
 
 @allure.step(title = 'test登录')
 def test_c():
